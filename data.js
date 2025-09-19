5b139fc060e2122f7d348a0cbfdc79a2144c7a35
Мария <128967641+kykareeky@users.noreply.github.com>
2025-09-19 08:24:32 +0300

    для лр4

---
+++ data.js
@@ -0,0 +1,67 @@
+// data.js
+const teas = [
+  {
+    keyword: "dian_hong",
+    name: "Дянь Хун",
+    price: 850,
+    category: "black",
+    count: "100г",
+    image: "teas/dian_hong.jpg"
+  },
+  {
+    keyword: "sencha",
+    name: "Сенча",
+    price: 750,
+    category: "green",
+    count: "100г",
+    image: "teas/sencha.jpg"
+  },
+  {
+    keyword: "tie_guan_yin",
+    name: "Улун Те Гуань Инь",
+    price: 920,
+    category: "oolong",
+    count: "100г",
+    image: "teas/tie_guan_yin.jpg"
+  },
+  {
+    keyword: "assam",
+    name: "Ассам",
+    price: 650,
+    category: "black",
+    count: "100г",
+    image: "teas/assam.jpg"
+  },
+  {
+    keyword: "shu_puer",
+    name: "Пуэр Шу",
+    price: 1100,
+    category: "puer",
+    count: "100г",
+    image: "teas/shu_puer.jpg"
+  },
+  {
+    keyword: "an_xi_tie_guan_yin",
+    name: "Анси Те Гуань Инь",
+    price: 890,
+    category: "oolong",
+    count: "100г",
+    image: "teas/an_xi_tie_guan_yin.jpg"
+  },
+  {
+    keyword: "long_jing",
+    name: "Лун Цзин",
+    price: 950,
+    category: "green",
+    count: "100г",
+    image: "teas/long_jing.jpg"
+  },
+  {
+    keyword: "da_hong_pao",
+    name: "Да Хун Пао",
+    price: 1200,
+    category: "oolong",
+    count: "100г",
+    image: "teas/da_hong_pao.jpg"
+  }
+];
